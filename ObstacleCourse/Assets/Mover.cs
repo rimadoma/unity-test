29b42e3f8de6512266ad15341040dfd57eb4360f
rimadoma <richard.domander@gmail.com>
2022-09-04 18:25:28 +0100

    Board, Movement, Follow camera,  collisions

--- ObstacleCourse/Assets/Mover.cs
+++ ObstacleCourse/Assets/Mover.cs
@@ -1,12 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xSpeed = 0.0f;
-    [SerializeField] float ySpeed = 0.01f;
-    [SerializeField] float zSpeed = 0.0f;
+    // Too high moveSpeed leads to player going through walls?
+    [SerializeField] float moveSpeed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +17,27 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(xSpeed, ySpeed, zSpeed);
+        Vector3 translation = getTranslation();
+        transform.Translate(translation);
+    }
+
+    private Vector3 getTranslation()
+    {
+        Vector3 input = readInput();
+
+        // Speed up with moveSpeed (dT is small)
+        input = input * moveSpeed;
+
+        // Make movement speed independent of FPS
+        return input * Time.deltaTime;
+    }
+
+    private Vector3 readInput()
+    {
+        return new Vector3
+        {
+            x = Input.GetAxis("Horizontal"),
+            z = Input.GetAxis("Vertical")
+        };
     }
 }
